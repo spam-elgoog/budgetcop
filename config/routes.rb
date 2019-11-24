91bb8f5f17d4ef031e833bf9e954a9609e356137
Peter Studnicka <peter.studnicka@shopify.com>
2019-11-23 21:31:07 -0500

    Add Categories Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/categories', to: 'categories#index'
+  get '/json/categories', to: 'categories#catjson'
 end
