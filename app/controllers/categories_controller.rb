151658884c7e46921eaff7b2ea2a9752d22bbe14
Peter Studnicka <peter.studnicka@shopify.com>
2019-11-23 21:40:02 -0500

    Update files with rubo cop rules

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Categories controller.
-# TODO: create a constant for these values for now until we decide to 
+# TODO: create a constant for these values for now until we decide to
 # if we should provide functionality to add categories via admin
 # Atm its unnecessary to get data from db.
 class CategoriesController < ApplicationController
@@ -13,7 +13,7 @@ class CategoriesController < ApplicationController
   def catjson
     @cats = Category.all
     # redirect_to categories_path
-    render 'index'
+    render('index')
     # render json: @cats.as_json(only: %i[id category short_desc full_desc])
   end
 end
--- db/migrate/20191117044820_create_budget_details.rb
+++ db/migrate/20191117044820_create_budget_details.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
 class CreateBudgetDetails < ActiveRecord::Migration[6.0]
   def change
     create_table :budget_details do |t|
-      t.references :category, null: false, foreign_key: true
-      t.references :budget_plan, null: false, foreign_key: true
-      t.decimal :amount, null: false, scale: 2, precision: 16
+      t.references(:category, null: false, foreign_key: true)
+      t.references(:budget_plan, null: false, foreign_key: true)
+      t.decimal(:amount, null: false, scale: 2, precision: 16)
 
       t.timestamps
     end
-    add_index :budget_details, %i[budget_plan_id category_id], unique: true
+    add_index(:budget_details, %i[budget_plan_id category_id], unique: true)
   end
 end
