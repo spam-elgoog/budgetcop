d6c1a88ddd5c31cab42e933b887af28fb7e5b41b
Peter Studnicka <peter.studnicka@shopify.com>
2019-11-25 20:47:34 -0500

    Add Authorization , Login, Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,33 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # for setting up a default page
+  root 'welcome#index'
+  # or rails < 4
+  # root to: 'welcome#index'
+  # get 'welcome/index'
 
+  #
+  # Users
+  #
+  resources :users
+
+  #
+  # Session and Authentication routes
+  # Remapping route
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :sessions
+  # get 'users/index'
+  # get 'users/new'
+  # get 'users/edit'
+  # get 'users/show'
+
+  #
+  # Categories
+  #
   get '/categories', to: 'categories#index'
   get '/json/categories', to: 'categories#catjson'
+
 end
