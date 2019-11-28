14d754ec200ae2d4682037db5b8ac1cae5275f79
Peter Studnicka <peter.studnicka@shopify.com>
2019-11-28 10:49:49 -0500

    Format code with Rubocop

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,24 @@
 # frozen_string_literal: true
-
 require_relative 'data_helper'
 
 # Categories Table
-Category.connection.execute 'ALTER TABLE `categories` AUTO_INCREMENT = 1'
+Category.connection.execute('ALTER TABLE `categories` AUTO_INCREMENT = 1')
 populate_categories
 
 # Users Table
-User.connection.execute 'ALTER TABLE `users` AUTO_INCREMENT = 1'
+User.connection.execute('ALTER TABLE `users` AUTO_INCREMENT = 1')
 populate_users
 
 # Budget_Plans Table
-BudgetPlan.connection.execute 'ALTER TABLE `budget_plans` AUTO_INCREMENT = 1'
+BudgetPlan.connection.execute('ALTER TABLE `budget_plans` AUTO_INCREMENT = 1')
 populate_plans
 
 # Budget_Details Table
 @plans = BudgetPlan.all
 BudgetDetail.connection
-            .execute 'ALTER TABLE `budget_details` AUTO_INCREMENT = 1'
-populate_details @plans
+  .execute('ALTER TABLE `budget_details` AUTO_INCREMENT = 1')
+populate_details(@plans)
 
 # Expenses Table
-Expense.connection.execute 'ALTER TABLE `expenses` AUTO_INCREMENT = 1'
-populate_expenses @plans
+Expense.connection.execute('ALTER TABLE `expenses` AUTO_INCREMENT = 1')
+populate_expenses(@plans)
--- db/utility.rb
+++ db/utility.rb
@@ -27,7 +27,7 @@
     [:damages, 'Damaged Goods',
      'Track damaged and lost items.'],
     [:misc, 'All Other Expenses',
-     'One time costs and items that just do not fit anywhere else.']
+     'One time costs and items that just do not fit anywhere else.'],
   ]
 
 @category_range =
@@ -43,19 +43,19 @@
       ads: 75..150, app: 75..150, refunds: 100..300, damages: 100..300,
       misc: 100..300, transportation: 200..400, permits: 200..400,
       storage: 800..1500, default: 20..100
-    }
+    },
   }
 
 def expense_amount(category)
   e = @category_range[:expenses]
-  random_amount e.key?(category) ? e[category] : e[:default]
+  random_amount(e.key?(category) ? e[category] : e[:default])
 end
 
 def budget_amount(category)
   b = @category_range[:budgets]
-  random_amount b.key?(category) ? b[category] : b[:default]
+  random_amount(b.key?(category) ? b[category] : b[:default])
 end
 
 def random_amount(range)
-  (rand(range) * 1.01).round 2
+  (rand(range) * 1.01).round(2)
 end
