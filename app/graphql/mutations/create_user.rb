bdf3d7b8e778be230ef348aa3b84c80b0786a5e4
Peter Studnicka <peter.studnicka@shopify.com>
2019-12-01 01:15:16 -0500

    Add graphql user attributes type

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -2,20 +2,14 @@
 
 module Mutations
   class CreateUser < BaseMutation
-    argument :f_name, String, required: true
-    argument :l_name, String, required: true
-    argument :email, String, required: true
-    argument :user_name, String, required: true
-    argument :password, String, required: true
-    argument :password_confirmation, String, required: true
     # this should be removed and adding admin
     # should be a different query
     # argument :admin, Boolean, required: false
-
+    argument :attributes, Types::Custom::UserAttributes, required: true
     field :user, Types::Custom::UserType, null: false
     field :errors, [String], null: false
-    def resolve(**args)
-      user = User.new(args)
+    def resolve(attributes:)
+      user = User.new(attributes.to_hash)
       if user.save
         {
           user: user,
